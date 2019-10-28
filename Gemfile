1329fca263774a5e5ebf7dc0492ff1dc12c7aebb
carter108 <nvtuong108@gmail.com>
2019-10-28 21:48:32 +0700

    Add test for models

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'will_paginate'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'shoulda-matchers', '~> 4.1', '>= 4.1.2'
   gem 'rspec-rails', '~> 3.9'
 end
 
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Video, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validates' do
+    it { should validate_presence_of(:link) }
+  end
+
+  describe 'associations' do
+    it { should belong_to(:user) }
+  end
 end
