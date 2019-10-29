6a553e50a27064e5350597972a3ac59235fc923b
carter108 <nvtuong108@gmail.com>
2019-10-29 22:07:30 +0700

    Allow user to login and register

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,23 @@
 class SessionsController < ApiController
+  before_action :find_user
   skip_before_action :authenticate, :only => [:create]
 
   def create
-    user = User.find_by(username: auth_params[:username])
-    if user && user.authenticate(auth_params[:password])
-      jwt = JsonWebToken.encode({ id: user.id})
-      render json: {token: jwt}, status: 201
+    if @user.present? && @user.authenticate(user_params[:password])
+      jwt = JsonWebToken.encode({ id: @user.id})
+      render json: {token: jwt, username: @user.username}, status: 201
     else
       unauthorized
     end
+    
   end
 
   private
-  def auth_params
-    params.require(:auth).permit(:username, :password)
+  def find_user
+    @user = User.find_by(username: user_params[:username]) || User.create(user_params)
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   post 'videos', to: 'videos#create'
   get 'videos', to: 'videos#index'
   get 'users', to: 'users#index'
-  post 'signup', to: 'users#create'
   post 'login', to: 'sessions#create'
 end
